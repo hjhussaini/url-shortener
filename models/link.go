24baab79e87ac52e8bbec08e64a8cf4d71d184c0
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-11-20 20:45:24 +0330

    models.Link
    Implement function Table()
    Implement function Fields()
    Implement function Insert()

--- models/link.go
+++ models/link.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/hjhussaini/url-shortener/database"
+)
 
 // swagger:model
 type Link struct {
@@ -10,4 +14,22 @@ type Link struct {
 	APIKey      string        `json:"api_key"`
 	UserName    string        `json:"user_name,omitempty"`
 	ExpireAt    time.Duration `json:"expire_at,omitempty"`
+	TTL         int64         `json:"-"`
+}
+
+func (link *Link) Table() string {
+	return "link"
+}
+
+func (link *Link) Fields() string {
+	return "short_url, original_url, api_key, user_name, expire_at"
+}
+
+func (link *Link) Insert(session database.Session) error {
+	return session.Insert(
+		link.TTL,
+		link.Table(),
+		link.Fields(),
+		link.ShortURL, link.OriginalURL, link.APIKey, link.UserName, link.ExpireAt,
+	)
 }

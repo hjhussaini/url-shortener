941fb71aff05c2b65c09af74ced966564549af09
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-11-20 16:57:29 +0330

    write-server
    Connect to database

--- write-server/main.go
+++ write-server/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/hjhussaini/url-shortener/database"
 	"github.com/hjhussaini/url-shortener/server"
 )
 
 func main() {
+	databaseServer := os.Getenv("DATABASE_SERVER")
+	databaseKeyspace := os.Getenv("DATABASE_KEYSPACE")
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
+
+	cassandra, err := database.CassandraConnect(databaseServer, databaseKeyspace)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer cassandra.Close()
 
 	server.RunHTTP(host+":"+port, nil)
 }

3fb51d5158f1035fe043728c26695873a47342cb
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-11-21 15:37:09 +0330

    cache.RedisCache
    Implement function Pop()

--- cache/redis.go
+++ cache/redis.go
@@ -15,6 +15,10 @@ func (cache *RedisCache) Push(key string, value string) error {
 	return cache.Client.LPush(cache.Context, key, value).Err()
 }
 
+func (cache *RedisCache) Pop(key string) (string, error) {
+	return cache.Client.LPop(cache.Context, key).Result()
+}
+
 func NewRedisCache(address string, database int) *RedisCache {
 	cache := &RedisCache{}
 	cache.Client = redis.NewClient(&redis.Options{

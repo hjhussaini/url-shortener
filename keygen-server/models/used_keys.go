3f7bff4ff9c4e1a3d607c638b3151e843127778c
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-11-23 15:45:31 +0330

    keygen-server/models.UsedKeys
    Implement function Insert()

--- keygen-server/models/used_keys.go
+++ keygen-server/models/used_keys.go
@@ -13,8 +13,21 @@ func (usedKeys *UsedKeys) Table() string {
 	return "used_keys"
 }
 
+func (usedKeys *UsedKeys) Fields() string {
+	return "key"
+}
+
 func (usedKeys *UsedKeys) Count() int64 {
 	count, _ := usedKeys.Session.Count(usedKeys.Table())
 
 	return count
+}
+
+func (usedKeys *UsedKeys) Insert() error {
+	return usedKeys.Session.Insert(
+		0,
+		usedKeys.Table(),
+		usedKeys.Fields(),
+		usedKeys.Key,
+	)
 }

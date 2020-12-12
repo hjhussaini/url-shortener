3afdd2ffd11bdceff44e92b488faf9cebd62f45a
Hamed Haji Hussaini <hajihussaini@afrabon.com>
2020-12-12 21:25:02 +0330

    link-server
    Connect to database server

--- link-server/main.go
+++ link-server/main.go
@@ -7,14 +7,25 @@ import (
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/hjhussaini/url-shortener/database"
 	"github.com/hjhussaini/url-shortener/link-server/api"
 	"github.com/hjhussaini/url-shortener/server"
 )
 
 func main() {
+	databaseServer := os.Getenv("DATABASE_SERVER")
+	databaseKeyspace := os.Getenv("DATABASE_KEYSPACE")
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 
-	links := api.Links{}
+	cassandra, err := database.CassandraConnect(databaseServer, databaseKeyspace)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer cassandra.Close()
+
+	links := api.Links{
+		Session: cassandra,
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/{short_link}", links.RedirectURL).Methods(http.MethodGet)
